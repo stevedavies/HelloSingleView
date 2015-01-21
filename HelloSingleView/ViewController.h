a19a7bb0c8eacfc7645f13b482b38eec290b1676
stevedavies <steve.davies.iphone@gmail.com>
2015-01-21 15:04:21 -0600

    New

---
+++ HelloSingleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloSingleView
+//
+//  Created by Steve Davies on 1/21/15.
+//  Copyright (c) 2015 steve davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloSingleView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloSingleView
+//
+//  Created by Steve Davies on 1/21/15.
+//  Copyright (c) 2015 steve davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
